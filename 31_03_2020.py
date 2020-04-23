ee05bc174115293ff19bc3f9dee0c178b8eb9720
MarceronT <58844411+MarceronT@users.noreply.github.com>
2020-04-23 10:58:09 +0200

    Update 31_03_2020.py

--- 31_03_2020.py
+++ 31_03_2020.py
@@ -12,11 +12,17 @@ while True:
     # y: y position of your pod
     # next_checkpoint_x: x position of the next check point
     # next_checkpoint_y: y position of the next check point
-    x, y, next_checkpoint_x, next_checkpoint_y, nextCheckpointDist, nextCheckpointAngle = [int(i) for i in input().split()]
+    (
+        x,
+        y,
+        next_checkpoint_x,
+        next_checkpoint_y,
+        nextCheckpointDist,
+        nextCheckpointAngle,
+    ) = [int(i) for i in input().split()]
     opponentX, opponentY = [int(i) for i in input().split()]
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
-
 
     # Edit this line to output the target position
     if nextCheckpointAngle > 90 or nextCheckpointAngle < -90:
@@ -26,3 +32,4 @@ while True:
     # and thrust (0 <= thrust <= 100)
     # i.e.: "x y thrust"
     print(str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " " + str(thrust))
+
